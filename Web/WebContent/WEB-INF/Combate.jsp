042ce7be2f3eb9900d1ca6db5cd1a19ba9fd9fe8
Juan Lucas <Juan Lucas@JuanLucas-PC>
2016-11-11 20:23:06 -0300

    Agregado del proyecto

---
+++ Web/WebContent/WEB-INF/Combate.jsp
@@ -0,0 +1,19 @@
+<%@page import="entidades.Personaje"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>War!!</title>
+</head>
+<body>
+	<h1>Combate!!</h1>
+	<% 
+		Personaje p1= ((Personaje)session.getAttribute("P1"));
+		Personaje p2= ((Personaje)session.getAttribute("P2"));
+	%>
+<%-- 	<%=p1.getNombre()+" "+p1.getApellido() %> --%>
+<%-- 	<%=p2.getNombre()+" "+p2.getApellido() %> --%>
+</body>
+</html>
---
+++ Web/src/servlets/Start.java
@@ -0,0 +1,63 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import logic.ControladorABMCPersonaje;
+import utils.ApplicationException;
+import entidades.*;
+
+/**
+ * Servlet implementation class Start
+ */
+@WebServlet("/Start")
+public class Start extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Start() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ControladorABMCPersonaje ctrl = new ControladorABMCPersonaje();
+		Personaje p1= new Personaje();
+		p1.setId(Integer.parseInt(request.getParameter("Personaje1")));
+		Personaje p2= new Personaje();
+		p2.setId(Integer.parseInt(request.getParameter("Personaje2")));
+		try {
+			p1=ctrl.busca(p1);
+			p2=ctrl.busca(p2);
+		} catch (ApplicationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//response.getWriter().append("P1: ").append(p1.getNombre()+" "+p1.getApellido());
+		//response.getWriter().append("P2: ").append(p2.getNombre()+" "+p2.getApellido());
+		request.getSession().setAttribute("P1", p1);
+		request.getSession().setAttribute("P2", p2);
+		//response.sendRedirect("WEB-INF/war.jsp");
+		request.getRequestDispatcher("WEB-INF/Combate.jsp").forward(request, response);
+		response.sendRedirect("Combate");
+	}
+
+}
